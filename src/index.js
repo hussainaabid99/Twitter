6a3dc45cee20086ad72d5ab1e20506ebde738a9e
Aabid <hussain.aabid2002@gmail.com>
2023-09-05 13:18:44 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import { connect } from './config/database.js';
+import apiRoutes from './routes/index.js';
+import bodyParser from 'body-parser';
+
 const app = express();
 
-import service from './service/tweet-service.js'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
 
 app.listen(3000, async () => {
           console.log('Server started');
           await connect();
           console.log('Mongo db connected');
-          // let ser = new service();
-          // await ser.create({ content: 'my other #CoDE #works or #NOT ?' });
-}); 
+});  
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -6,7 +6,7 @@ const tweetSchema = new mongoose.Schema({                                       
                     required: true,
                     max: [250, 'Tweet cannot be more than 250 characters']
 
-          },
+          }
 
 }, { timestamps: true });
 
