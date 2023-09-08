925eeb35a099baf410df27c4f3073768c7a6f278
Aabid <hussain.aabid2002@gmail.com>
2023-09-08 23:47:11 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -2,29 +2,24 @@ import express from 'express';
 import { connect } from './config/database.js';
 import apiRoutes from './routes/index.js';
 import bodyParser from 'body-parser';
+import passport from 'passport';
+
+import { passportAuth } from './config/jwt-middleware.js'
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(passport.initialize());
+passportAuth(passport);
+
 app.use('/api', apiRoutes);
 
-import { UserRepository, TweetRepository } from './repository/index.js'
-import LikeService from './service/like-service.js';
 
 app.listen(3000, async () => {
           console.log('Server started');
           await connect();
           console.log('Mongo db connected');
 
-          // const userRepo = new UserRepository();
-          // const tweetRepo = new TweetRepository();
-          // const tweets = await tweetRepo.getAll(0, 10);
-
-
-          // const users = await userRepo.getAll();
-
-          // const likeService = new LikeService();
-          // await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
-});   
+});
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -15,6 +15,37 @@ class UserService {
                     }
 
           }
+
+          async getUserByEmail(email) {
+                    try {
+                              const user = await this.userRepository.findBy({ email })
+                              return user;
+                    } catch (error) {
+                              throw error;
+                    }
+          }
+
+          async signin(data) {
+                    try {
+                              const user = await this.getUserByEmail(data.email);
+                              if (!user) {
+                                        throw {
+                                                  message: 'no user found',
+                                                  success: false,
+                                        };
+                              }
+                              if (!user.comparePassword(data.password)) {
+                                        throw {
+                                                  message: 'Incorrect password',
+                                                  success: false
+                                        };
+                              }
+                              const token = user.genJWT();
+                              return token;
+                    } catch (error) {
+                              throw error;
+                    }
+          }
 }
 
 export default UserService;
